dbfbc9b9749c3064be5938c78c7986a36ae8b5db
Metehan Yaka <72347062+mete20@users.noreply.github.com>
2023-11-16 16:06:48 +0300

    javafx setup, app package

--- src/main/java/com/KUAlchemists/backend/AlchemistsGame.java
+++ src/main/java/com/KUAlchemists/backend/AlchemistsGame.java
@@ -1,9 +1,6 @@
 package com.KUAlchemists.backend;
 
 public class AlchemistsGame {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
 
     public String FizzBuzz(int number) {
         return "1";
