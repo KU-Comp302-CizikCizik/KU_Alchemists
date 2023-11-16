8242f83b86b8818ce85e0796a496ae1811ce79ee
Metehan Yaka <72347062+mete20@users.noreply.github.com>
2023-11-16 15:27:51 +0300

    Basic Test Codes

--- src/main/java/com/KUAlchemists/backend/AlchemistsGame.java
+++ src/main/java/com/KUAlchemists/backend/AlchemistsGame.java
@@ -1,4 +1,11 @@
 package com.KUAlchemists.backend;
 
 public class AlchemistsGame {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public String FizzBuzz(int number) {
+        return "1";
+    }
 }
--- src/test/java/com/KUAlchemists/backend/AlchemistsGameTest.java
+++ src/test/java/com/KUAlchemists/backend/AlchemistsGameTest.java
@@ -1,7 +1,17 @@
 package com.KUAlchemists.backend;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
-class AlchemistsGameTest {
+
+
+public class AlchemistsGameTest {
+
+    @Test
+    public void FizzBuzzNormalNumbers() {
+
+        AlchemistsGame alchemistsGame = new AlchemistsGame();
+        Assertions.assertEquals("1", alchemistsGame.FizzBuzz(1));
+    }
 
 }
