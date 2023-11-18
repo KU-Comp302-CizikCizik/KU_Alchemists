9ac15f690ff09e012aeb863c041a2a186fb9327b
emirhancakirr <ecakir20@ku.edu.tr>
2023-11-18 23:23:36 +0300

    her şey tamam ama build edereken pathtten dolayı sıkıntı cıkarıyor

--- src/main/java/com/KUAlchemists/backend/controllers/LoginController.java
+++ src/main/java/com/KUAlchemists/backend/controllers/LoginController.java
@@ -1,13 +1,39 @@
 package com.KUAlchemists.backend.controllers;
 
 import com.KUAlchemists.backend.services.LoginService;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 
+import java.awt.*;
 import java.util.Properties;
 
 /**
  * This class is responsible for handling login requests.
  */
 public class LoginController {
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private TextField usernameTextField;
+
+    @FXML
+    private TextField passwordTextField;
+
+    @FXML
+    private Label loginMessageLabel;
+
+    public void loginButtonOnAction(ActionEvent event) {
+        if (usernameTextField.getText().isBlank() == false && passwordTextField.getText().isBlank() == false) {
+            validateUser();
+        } else {
+            loginMessageLabel.setText("Please enter username and password.");
+        }
+    }
+
+    private void validateUser() {
+
+    }
 
     /**
      * The LoginService that this controller uses to validate users.
--- src/main/java/com/KUAlchemists/ui/LoginPageUI.java
+++ src/main/java/com/KUAlchemists/ui/LoginPageUI.java
@@ -1,13 +1,17 @@
 package com.KUAlchemists.ui;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 
 public class LoginPageUI extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Login - KU Alchemists");
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("../../../../resources/LoginPageUI.fxml"));
+        primaryStage.setTitle("KU Alchemists");
+        primaryStage.setScene(new Scene(root, 320, 240));
         primaryStage.show();
-
-    }
+}
 }
