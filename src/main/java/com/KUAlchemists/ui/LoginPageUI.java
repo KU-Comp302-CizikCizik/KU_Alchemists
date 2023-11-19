2197a50f76064d53f958155e1777dbbb15944d76
emirhancakirr <ecakir20@ku.edu.tr>
2023-11-19 12:49:13 +0300

    maingame eklendi ama hala fxmli loadlaymıyor

--- src/main/java/com/KUAlchemists/ui/LoginPageUI.java
+++ src/main/java/com/KUAlchemists/ui/LoginPageUI.java
@@ -9,7 +9,7 @@ import javafx.scene.Scene;
 public class LoginPageUI extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("../../../../resources/LoginPageUI.fxml"));
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("MainGame.fxml"));
         primaryStage.setTitle("KU Alchemists");
         primaryStage.setScene(new Scene(root, 320, 240));
         primaryStage.show();
--- src/main/java/com/KUAlchemists/ui/MainGameUI.java
+++ src/main/java/com/KUAlchemists/ui/MainGameUI.java
@@ -11,13 +11,9 @@ public class MainGameUI extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        //FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("MainGame.fxml"));
-        //Parent root = fxmlLoader.load();
-        //Scene scene = new Scene(root, 320, 240);
-//        primaryStage.setScene(scene);
-
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("MainGameUI.fxml"));
         primaryStage.setTitle("KU Alchemists");
+        primaryStage.setScene(new Scene(root, 320, 240));
         primaryStage.show();
-
     }
 }
