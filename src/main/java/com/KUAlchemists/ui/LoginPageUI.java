aea1d77efe91c9172cbfe381532b5f1281f6c3f8
Metehan Yaka <72347062+mete20@users.noreply.github.com>
2023-11-26 21:23:32 +0300

    could not found main fixed

--- src/main/java/com/KUAlchemists/ui/LoginPageUI.java
+++ src/main/java/com/KUAlchemists/ui/LoginPageUI.java
@@ -2,6 +2,7 @@ package com.KUAlchemists.ui;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -11,9 +12,9 @@ public class LoginPageUI extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource("LoginPageUI.fxml"));
-        fxmlLoader.setRoot(new AnchorPane());
-        Parent root = fxmlLoader.load();
-
+        BorderPane root = fxmlLoader.load();
+        //fxmlLoader.setRoot(new AnchorPane());
+        fxmlLoader.setRoot(root);
 
         primaryStage.setTitle("KU Alchemists");
         primaryStage.setScene(new Scene(root, 320, 240));
