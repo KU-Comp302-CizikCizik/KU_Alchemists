e0a0bb8c254861e5b121b19f3c22a68e6cbdd83f
Metehan Yaka <72347062+mete20@users.noreply.github.com>
2023-12-07 00:00:39 +0300

    getPublishedTheoriesInfo implemented

--- src/test/java/com/KUAlchemists/backend/services/PublicationServiceTest.java
+++ src/test/java/com/KUAlchemists/backend/services/PublicationServiceTest.java
@@ -4,6 +4,10 @@ import com.KUAlchemists.backend.models.*;
 import com.KUAlchemists.backend.enums.Aspect;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
@@ -58,6 +62,27 @@ class PublicationServiceTest {
         verify(playerService, never()).updatePlayerReputation(anyString(), anyInt());
     }
 
-    // Additional tests for edge cases and failure scenarios...
+    @Test
+    void getPublishedTheoriesInfo_ReturnsCorrectInfo() {
+        // Arrange
+        Alchemical alc1 = new Alchemical(Aspect.POSITIVE_BIG, Aspect.NEGATIVE_SMALL, Aspect.POSITIVE_SMALL);
+        Alchemical alc2 = new Alchemical(Aspect.NEGATIVE_BIG, Aspect.POSITIVE_SMALL, Aspect.NEGATIVE_SMALL);
+        Theory theory1 = new Theory(new Ingredient("Wolfsbane",alc1 ), alc1);
+        Theory theory2 = new Theory(new Ingredient("Nightshade",alc2 ), alc2);
+        List<Theory> publishedTheories = Arrays.asList(theory1, theory2);
+
+        when(player.getPublishedTheories()).thenReturn(publishedTheories);
+
+        String expectedInfo = "Wolfsbane POSITIVE_BIG NEGATIVE_SMALL POSITIVE_SMALL\n" +
+                "Nightshade NEGATIVE_BIG POSITIVE_SMALL NEGATIVE_SMALL\n";
+
+        // Act
+        String actualInfo = publicationService.getPublishedTheoriesInfo(player);
+
+        // Assert
+        assertEquals(expectedInfo, actualInfo);
+    }
+
+
 }
 
