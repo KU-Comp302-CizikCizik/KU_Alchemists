02e14680f63f0b707b696ed68403785d7ba188cc
Metehan Yaka <72347062+mete20@users.noreply.github.com>
2023-12-07 23:34:39 +0300

    HelpScreen related docs added

--- src/main/java/com/KUAlchemists/backend/services/HelpScreenService.java
+++ src/main/java/com/KUAlchemists/backend/services/HelpScreenService.java
@@ -5,20 +5,31 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
+/**
+ * This class is responsible for storing the help screen content.
+ */
 public class HelpScreenService {
-
+    /**
+     * The content of the help screen.
+     */
     private String helpContent;
-
+    /**
+     * Constructor for HelpScreenService
+     */
     public HelpScreenService(){
         setHelpContent();
     }
 
+    /**
+     * Sets the content of the help screen.
+     */
     private void setHelpContent(){
-        // read the csv file and set the content
+        // read the txt file and set the content
         this.helpContent = "";
         try (InputStream is = getClass().getClassLoader().getResourceAsStream("helpContent.txt");
              BufferedReader reader = new BufferedReader(new InputStreamReader(is))) {
             String line;
+            // read the txt file line by line
             while ((line = reader.readLine()) != null) {
                 helpContent += line;
             }
@@ -26,6 +37,12 @@ public class HelpScreenService {
             e.printStackTrace();
         }
     }
+
+    /**
+     * Gets the content of the help screen.
+     *
+     * @return The content of the help screen.
+     */
     public String getHelpContent(){
         return this.helpContent;
     }
