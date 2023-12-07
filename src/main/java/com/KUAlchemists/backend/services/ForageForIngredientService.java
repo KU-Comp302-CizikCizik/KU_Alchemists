a371ce099bab5d9567341a56df533f5b7fd56972
Metehan Yaka <72347062+mete20@users.noreply.github.com>
2023-12-07 23:28:29 +0300

    forageForIngredient related docs added

--- src/main/java/com/KUAlchemists/backend/services/ForageForIngredientService.java
+++ src/main/java/com/KUAlchemists/backend/services/ForageForIngredientService.java
@@ -5,22 +5,39 @@ import com.KUAlchemists.backend.models.Deck;
 import com.KUAlchemists.backend.models.Ingredient;
 import com.KUAlchemists.backend.models.Player;
 
+/**
+ * This class is responsible for storing the ingredients in the game.
+ */
 public class ForageForIngredientService {
-
+    /**
+     * The singleton instance of deck.
+     */
     private final Deck deck = Deck.getInstance();
-    public ForageForIngredientService( ) {
+    /**
+     * Constructor for IngredientStorageService
+     */
 
+    public ForageForIngredientService( ) {
     }
 
+    /**
+     * Forages for an ingredient.
+     *
+     * @param player
+     * @return
+     */
     public String forageForIngredient(Player player){
         Ingredient ingredient;
         try {
-             ingredient = deck.drawIngredient();
+            //draw an ingredient from the deck
+            ingredient = deck.drawIngredient();
+            //add the ingredient to the player's storage
             Board.getInstance().getIngredientStorage(player).addIngredient(ingredient);
         }
         catch (RuntimeException e){
             return "ERROR! Deck is empty";
         }
+        //return the name of the ingredient
         return ingredient.getName().toLowerCase();
     }
 }
