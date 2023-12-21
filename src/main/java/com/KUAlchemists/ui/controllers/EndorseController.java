f549c58596a73afad77c6897aa0646bec56f119d
emirhancakirr <ecakir20@ku.edu.tr>
2023-12-22 00:06:12 +0300

    red seal images are added

--- src/main/java/com/KUAlchemists/ui/controllers/EndorseController.java
+++ src/main/java/com/KUAlchemists/ui/controllers/EndorseController.java
@@ -1,4 +1,73 @@
 package com.KUAlchemists.ui.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
 public class EndorseController {
+
+    @FXML
+    private ImageView alchemy;
+
+    @FXML
+    private Text endorseButton;
+
+    @FXML
+    private ImageView seal1;
+
+    @FXML
+    private ImageView seal2;
+
+    @FXML
+    private ImageView seal3;
+
+    @FXML
+    private ImageView selfSeal1;
+
+    @FXML
+    private ImageView selfSeal2;
+
+    @FXML
+    private ImageView selfSeal3;
+
+    @FXML
+    private ImageView selfSeal4;
+
+    @FXML
+    private ImageView selfSeal5;
+
+    @FXML
+    private ImageView theory;
+
+    @FXML
+    void endorseClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void seal1Clicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void seal2Clicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void seal3Clicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void seal4Clicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void seal5Clicked(MouseEvent event) {
+
+    }
+
 }
