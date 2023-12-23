4167a9e616473f0fa8d76a00539d05f2259f4a8b
ekaramustafa20 <ekaramustafa20@ku.edu.tr>
2023-12-23 20:55:50 +0300

    Handler can get the seals of endorses

--- src/main/java/com/KUAlchemists/backend/handlers/EndorseHandler.java
+++ src/main/java/com/KUAlchemists/backend/handlers/EndorseHandler.java
@@ -1,11 +1,14 @@
 package com.KUAlchemists.backend.handlers;
 
 import com.KUAlchemists.backend.engine.GameEngine;
+import com.KUAlchemists.backend.enums.PlayerSeal;
 import com.KUAlchemists.backend.enums.TheorySeal;
+import com.KUAlchemists.backend.models.Player;
 import com.KUAlchemists.backend.models.Theory;
 import com.KUAlchemists.backend.observer.PublicationTrackObserver;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -26,21 +29,27 @@ public class EndorseHandler implements PublicationTrackObserver {
     }
 
     public ArrayList<String> getSeals() {
-        ArrayList<String> seals =  GameEngine.getInstance().getCurrentPlayer().getTheorySeals()
+        ArrayList<String> theorySeals =  GameEngine.getInstance().getCurrentPlayer().getTheorySeals()
                 .stream()
                 .map(TheorySeal::getSealString)
                 .collect(Collectors.toCollection(ArrayList::new));
-        Set<String> set = new HashSet<>(seals);
+        Set<String> set = new HashSet<>(theorySeals);
         return new ArrayList<>(set);
     }
 
     public ArrayList<String> getEndorsedSeals() {
-        ArrayList<String> seals = new ArrayList<>();
-        //I am placing them as secretSeal
-        seals.add("blue"); //blue secret seal
-        seals.add("green"); //gree secret seal
+        //for testing purposes
+        //
+        selectedTheory = new Theory();
+        ArrayList<Player> temp = new ArrayList<>();
+        temp.add(GameEngine.getInstance().getCurrentPlayer());
+        selectedTheory.setEndorsers(temp);
+        //
+        ArrayList<String> playerSeals =  selectedTheory.getEndorsers()
+                .stream().map(player -> player.getPlayerSeal().getSealString())
+                .collect(Collectors.toCollection(ArrayList::new));
 
-        return seals;
+        return playerSeals;
     }
 
     public void saveEndorsedSeal(String name) {
