7b540af23e7b5ec53ccb48c056adf55f3b70be8a
aus20 <aus20@ku.edu.tr>
2023-12-28 00:05:36 +0300

    some hot fix to publish before starting endorse

--- src/main/java/com/KUAlchemists/backend/services/PublishTheoryService.java
+++ src/main/java/com/KUAlchemists/backend/services/PublishTheoryService.java
@@ -2,7 +2,14 @@ package com.KUAlchemists.backend.services;
 
 import com.KUAlchemists.backend.engine.GameEngine;
 import com.KUAlchemists.backend.enums.Aspect;
+import com.KUAlchemists.backend.enums.TheorySeal;
 import com.KUAlchemists.backend.models.*;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class PublishTheoryService {
 
     public PublishTheoryService() {
@@ -45,5 +52,64 @@ public class PublishTheoryService {
             return true;
         }
         return false;
+    }
+
+    /**
+     * This method retrieves the seals of the player.
+     *
+     * @return List of seal strings.
+     */
+    public ArrayList<String> getPlayerTheorySeals() {
+        ArrayList<String> theorySeals = GameEngine.getInstance().getCurrentPlayer().getTheorySeals()
+                .stream()
+                .map(TheorySeal::getSealString)
+                .collect(Collectors.toCollection(ArrayList::new));
+        // Remove duplicates
+        Set<String> set = new HashSet<>(theorySeals);
+        return new ArrayList<>(set);
+    }
+
+    /**
+     * This method retrieves the seals of the endorsed theory.
+     *
+     * @return List of seal strings.
+     */
+    public ArrayList<String> getEndorsedTheorySeals(Theory theory) {
+        ArrayList<String> playerSeals = theory.getEndorsers()
+                .stream()
+                .map(player -> player.getPlayerSeal().getSealString())
+                .collect(Collectors.toCollection(ArrayList::new));
+
+        return playerSeals;
+    }
+    /**
+     * This method saves the endorsed seal for the theory.
+     *
+     * @param sealName The name of the seal to be saved.
+     */
+    public void saveEndorsedSeal(Theory theory, String sealName) {
+        // The format SealGS SealSS SealRQ SealBQ SealGQ
+        TheorySeal seal = TheorySeal.getSealByName(sealName);
+        GameEngine.getInstance().getCurrentPlayer().removeTheorySeal(seal);
+        theory.addEndorser(GameEngine.getInstance().getCurrentPlayer());
+        Board.getInstance().updateTheTheory(theory);
+    }
+
+    /**
+     * This method retrieves the theory name.
+     *
+     * @return The name of the theory.
+     */
+    public String getTheoryName(Theory theory) {
+        return theory.getIngredient().getName().toLowerCase();
+    }
+
+    /**
+     * This method retrieves the player's seal.
+     *
+     * @return The player's seal string.
+     */
+    public String getPlayerSeal() {
+        return GameEngine.getInstance().getCurrentPlayer().getPlayerSeal().getSealString();
     }
 }
