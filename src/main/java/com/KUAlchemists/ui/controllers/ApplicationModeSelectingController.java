5607d613f13fd2f8ca9dca99670eee97e714036d
ekaramustafa20 <ekaramustafa20@ku.edu.tr>
2024-01-07 16:41:29 +0300

    small bug fixed

--- src/main/java/com/KUAlchemists/ui/controllers/ApplicationModeSelectingController.java
+++ src/main/java/com/KUAlchemists/ui/controllers/ApplicationModeSelectingController.java
@@ -4,7 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
-public class GameModeSelectingController {
+public class ApplicationModeSelectingController {
 
     @FXML
     private Button offline_b;
