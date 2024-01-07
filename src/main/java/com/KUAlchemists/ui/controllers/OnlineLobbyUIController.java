7d94e3de17771a9ddfa60d162def36e5304726a4
arday <ayeni20@ku.edu.tr>
2024-01-07 18:45:10 +0300

    some hotfix

---
+++ src/main/java/com/KUAlchemists/ui/controllers/OnlineLobbyUIController.java
@@ -0,0 +1,16 @@
+package com.KUAlchemists.ui.controllers;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class OnlineLobbyUIController {
+
+    @FXML
+    private Button bttn1;
+
+    @FXML
+    void selected(ActionEvent event) {
+
+    }
+
+}
