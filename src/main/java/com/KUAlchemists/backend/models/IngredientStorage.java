e2bb6fade4497224e4bbc34f0d40fc1c5bbf61e9
ekaramustafa20 <ekaramustafa20@ku.edu.tr>
2024-01-07 12:12:49 +0300

    abstraction and specification done

--- src/main/java/com/KUAlchemists/backend/models/IngredientStorage.java
+++ src/main/java/com/KUAlchemists/backend/models/IngredientStorage.java
@@ -7,24 +7,33 @@ import java.util.ArrayList;
  *
  */
 public class IngredientStorage {
+    //OVERVIEW: IngredientStorage is an immutable collection of ingredients that are available in the game.
 
     /**
      * List of ingredients in the storage.
      */
-    private ArrayList<Ingredient> ingredientsList = new ArrayList<Ingredient>();
+    private ArrayList<Ingredient> ingredientsList;
 
     /**
      * Constructor for IngredientStorage
      */
+    //constructor
     public IngredientStorage(){
+        //@effects: creates an ingredientList
+        ingredientsList = new ArrayList<Ingredient>();
     }
 
+    //methods
     /**
      * Adds an ingredient to the storage.
      *
      * @param ingredient The ingredient to be added.
      */
     public void addIngredient(Ingredient ingredient){
+        //Partial Procedure
+        //@requires: ingredient != null
+        //@modifies: this
+        //@effects: adds the ingredient to the storage
         ingredientsList.add(ingredient);
     }
 
@@ -34,6 +43,10 @@ public class IngredientStorage {
      * @param ingredient The ingredient to be removed.
      */
     public void removeIngredient(Ingredient ingredient){
+        //Partial Procedure
+        //@requires: ingredient != null
+        //@modifies: this
+        //@effects: removes the ingredient from the storage
         ingredientsList.remove(ingredient);
     }
 
@@ -43,6 +56,10 @@ public class IngredientStorage {
      * @param ingredientName The name of the ingredient to be removed.
      */
     public void removeIngredientByName(String ingredientName){
+        //Partial procedure
+        //@requires: ingredientName != null
+        //@modifies: this
+        //@effects: removes the ingredient from the storage
         for (Ingredient ingredient : ingredientsList) {
             if (ingredientName.equals(ingredient.getName())) {
                 ingredientsList.remove(ingredient);
@@ -57,6 +74,8 @@ public class IngredientStorage {
      * @return The ingredients in the storage.
      */
     public ArrayList<Ingredient> getIngredientsList(){
+        //Total procedure
+        //@effects: returns the ingredients in the storage
         return ingredientsList;
     }
 
@@ -67,14 +86,16 @@ public class IngredientStorage {
      * @return The ingredient with the given name.
      */
     public Ingredient getIngredient(String name) {
+        //Partial procedure
+        //@requires: name != null
+        //@effects: returns the ingredient with the given name
         for (Ingredient ingredient : ingredientsList) {
-            if (name.equals(ingredient.getName())) {
+            if (ingredient.getName().equals(name)) {
                 return ingredient;
             }
         }
         return null;
     }
-
 
 
 
