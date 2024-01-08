50a4fbf7e0fd1f3d07efb0c635e8049099a78556
mdeniz20 <91716426+mdeniz20@users.noreply.github.com>
2024-01-08 21:44:30 +0300

    UseArtifact UI is done. Some handler functions needed to be implemented.

--- src/main/java/com/KUAlchemists/backend/handlers/UseArtifactHandler.java
+++ src/main/java/com/KUAlchemists/backend/handlers/UseArtifactHandler.java
@@ -5,10 +5,13 @@ import com.KUAlchemists.backend.models.Player;
 import com.KUAlchemists.backend.services.UseArtifactService;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class UseArtifactHandler {
     private final UseArtifactService useArtifactService;
+
+    private static ArrayList<String> usedArtifacts = new ArrayList<>();
     private static UseArtifactHandler INSTANCE;
 
     private UseArtifactHandler() {
@@ -35,12 +38,17 @@ public class UseArtifactHandler {
     //new:: returns used artifacts list.
     public List<String> handleUsedArtifacts(){
 
-        return useArtifactService.getUsedArtifacts();
+//        return useArtifactService.getUsedArtifacts();
 
+        return usedArtifacts;
+    }
+    public List<String> handleGetAllArtifacts(){
+        return Arrays.asList("elixir_of_insight", "philosophers_compass", "magic_mortar", "printing_press", "wisdom_idol");
     }
     public void handleRemoveArtifact(String name){
         Player currentPlayer = GameEngine.getInstance().getCurrentPlayer();
         useArtifactService.removeArtifactFromStorage(name, currentPlayer);
+        usedArtifacts.add(name);
     }
     public List <String> handleStorageArtifact(){
         //return buyArtifactService.getBoughtArtifacts();
